3785843174fec4643eb751417ffffa5df78ca9c0
Sam <sam.park1993@gmail.com>
2020-12-06 18:27:23 -0800

    adding toggle header to list page, allow routing

--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -2,20 +2,24 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 
+import ToggleHeader from "../../components/ToggleHeader/ToggleHeader";
 import styles from "./List.module.css";
 
 function ListView({ nonprofits }) {
   return (
-    <List>
-      {nonprofits.map(({ name, summary, logo }) => (
-        <ListItem>
-          <img className={styles.icon} src="/logo192.png" />
-          {name}
-          {summary}
-          <ChevronRightIcon className={styles.chevron} />
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <ToggleHeader />
+      <List>
+        {nonprofits.map(({ name, summary, logo }) => (
+          <ListItem>
+            <img className={styles.icon} src="/logo192.png" />
+            {name}
+            {summary}
+            <ChevronRightIcon className={styles.chevron} />
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 }
 export default ListView;
