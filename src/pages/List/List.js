5b0bd4a75ef9c681d79f8cd1975b05525915ac39
Brandon J Wong <branjwong@gmail.com>
2020-12-06 18:24:08 -0800

    fix formating on name text for list

--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -1,5 +1,7 @@
+import Divider from "@material-ui/core/Divider";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import Typography from "@material-ui/core/Typography";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 
 import styles from "./List.module.css";
@@ -7,13 +9,18 @@ import styles from "./List.module.css";
 function ListView({ nonprofits }) {
   return (
     <List>
-      {nonprofits.map(({ name, summary, logo }) => (
-        <ListItem>
-          <img className={styles.icon} src="/logo192.png" />
-          {name}
-          {summary}
-          <ChevronRightIcon className={styles.chevron} />
-        </ListItem>
+      {nonprofits.map(({ name, summary, logo }, index) => (
+        <>
+          {index > 0 && <Divider />}
+          <ListItem className={styles.listItem}>
+            <img className={styles.icon} src="/logo192.png" />
+            <div className={styles.text}>
+              <Typography className={styles.name}>{name}</Typography>
+              <Typography>{summary}</Typography>
+            </div>
+            <ChevronRightIcon className={styles.chevron} />
+          </ListItem>
+        </>
       ))}
     </List>
   );
